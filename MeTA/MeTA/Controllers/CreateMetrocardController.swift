4395552d8c7164115f282474e8b23a161cad2595
Salman Siraj <ss10298@nyu.edu>
2020-02-24 01:24:25 -0500

    new changes for refill UI done

--- MeTA/MeTA/Controllers/CreateMetrocardController.swift
+++ MeTA/MeTA/Controllers/CreateMetrocardController.swift
@@ -10,9 +10,44 @@
 import UIKit
 
 
-class CreateMetrocardController: UIViewController {
+class CreateMetrocardController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var continueButton: UIButton!
+    @IBOutlet weak var coverBackground: UIImageView!
+    
+    let metroCardTypes = [
+        "Direct Deposit",
+        "Weekly Card",
+        "Monthly Card"
+    ]
+    
+    // Number of columns of data
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return metroCardTypes.count
+    }
+    
+    // The data to return fopr the row and component (column) that's being passed in
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return metroCardTypes[row]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.pickerView.dataSource = self
+        self.pickerView.delegate = self
+    }
+    @IBAction func buttonPressed(_ sender: Any) {
+        let selectedValue = metroCardTypes[pickerView.selectedRow(inComponent: 0)]
+        
+        if selectedValue == "Direct Deposit" {
+            coverBackground.isHidden = true
+        }
     }
 }
