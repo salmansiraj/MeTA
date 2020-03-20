4fe28179f82f9bc37862bcfd71ca574c17f8c62a
Salman Siraj <ss10298@nyu.edu>
2020-03-20 01:14:15 -0400

    square payment card feature implemented for mock cards

---
+++ MeTA/MeTA/Views/Generators/Style.swift
@@ -0,0 +1,15 @@
+//
+//  Style.swift
+//  MeTA
+//
+//  Created by salman siraj on 3/19/20.
+//  Referenced From © 2018 Square, Inc.
+//
+
+import UIKit
+
+struct Style {
+    static func setDefaults() {
+        UINavigationBar.appearance().tintColor = Color.primaryAction
+    }
+}
