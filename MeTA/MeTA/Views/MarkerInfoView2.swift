84543a775d37a53d0ac71bc212917c2e0a9accf0
Kevin J. Zheng <kjz233@nyu.edu>
2020-04-01 19:10:15 -0400

    file reference fixes

---
+++ MeTA/MeTA/Views/MarkerInfoView2.swift
@@ -0,0 +1,15 @@
+//
+//  MarkerInfoView.swift
+//  MeTA
+//
+//  Created by Kevin J. Zheng on 2/29/20.
+//  Copyright © 2020 senior design. All rights reserved.
+//
+
+import UIKit
+
+class MarkerInfoView: UIView {
+  
+  @IBOutlet weak var placePhoto: UIImageView!
+  @IBOutlet weak var nameLabel: UILabel!
+}
